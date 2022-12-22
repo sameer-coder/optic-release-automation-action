59ca022bbe58ca246a3eb840c8497bc9c6f3cc5f
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-12-22 18:36:27 +0530

    test

--- src/utils/releaseNotes.js
+++ src/utils/releaseNotes.js
@@ -5,7 +5,7 @@ const _truncate = require('lodash.truncate')
 
 const md = require('markdown-it')()
 
-const PR_BODY_TRUNCATE_SIZE = 30000
+const PR_BODY_TRUNCATE_SIZE = 40000
 
 function getPrNumbersFromReleaseNotes(releaseNotes) {
   const parsedReleaseNotes = md.parse(releaseNotes, {})
